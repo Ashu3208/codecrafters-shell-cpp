d9a108273e4228f0b13552a220ea46bc0ea1a61f
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2025-02-27 08:26:37 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <sstream>
+#include<vector>
+using namespace std;
 
 int main()
 {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  // Uncomment this block to pass the first stage
   bool end_program = false;
   while (!end_program)
   {
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    if(input == "exit 0")return 0;
-    std::cout << input << ": command not found" << std::endl;
+    // Taking the user input
+    cout << "$ ";
+    string input;
+    getline(cin, input);
+
+    stringstream s(input);
+    vector<string> arguments;
+    string argument;
+    while (s >> argument)
+    {
+      arguments.push_back(argument);
+    }
+
+    if(arguments[0]=="exit"){
+      if(arguments.size() <=1) cerr<<"exit code not provided"<<endl;
+      if (arguments[1] == "0") return 0;
+    }
+    else if(arguments[0]=="echo"){
+      for(int i=1;i<arguments.size();i++) cout<<arguments[i]<<" ";
+      cout<<endl;
+    }
+    else{
+      std::cout << input << ": command not found" << std::endl;
+    }
+
   }
 }
