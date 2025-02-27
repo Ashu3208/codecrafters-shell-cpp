63b6c1b6ca2393b712454b282d17b51eeac6d2aa
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2025-02-27 18:16:32 +0530

    builtin commands and path
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    [any message]
    
    support for quoting with double quotes
    
    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,120 @@
 #include <iostream>
 #include <sstream>
-#include<vector>
+#include <vector>
 #include <unordered_set>
 #include <cstdlib>
 #include <filesystem>
 #include <unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
-string find_executable(string exe_path, vector<string>exec_dirs){
-  for(auto dir: exec_dirs){
+unordered_set<string> builtins{"echo", "exit", "type", "pwd", "cd"};
+vector<string> exec_dirs;
+
+vector<string> parse_arguments(const string &input)
+{
+  vector<string> arguments;
+  string argument;
+  bool in_quotes = false, in_double_quotes = false;
+
+  for (size_t i = 0; i < input.length(); i++)
+  {
+    char c = input[i];
+    if (c == '"' and !in_quotes)
+    {
+      in_double_quotes = !in_double_quotes;
+      continue;
+    }
+    if (c == '\'' and !in_double_quotes)
+    {
+      in_quotes = !in_quotes; // Toggle quote state
+      continue;               // Don't include the quote itself
+    }
+
+    if (!in_quotes && !in_double_quotes && c == ' ')
+    {
+      if (!argument.empty())
+      {
+        arguments.push_back(argument);
+        argument.clear();
+      }
+    }
+    else
+    {
+      argument += c;
+    }
+  }
+
+  if (!argument.empty())
+  {
+    arguments.push_back(argument); // Push last token
+  }
+
+  return arguments;
+}
+
+string find_executable(string exe_path, vector<string> exec_dirs)
+{
+  for (auto dir : exec_dirs)
+  {
     string path = dir + "/" + exe_path;
-    if(std::filesystem::exists(path)){
+    if (std::filesystem::exists(path))
+    {
       return path;
     }
   }
   return "";
+}
+
+void run_builtin_commands(string command, vector<string> &args)
+{
+  if (command == "pwd")
+  {
+    cout << getcwd(nullptr, 0) << endl;
+  }
+  else if (command == "type")
+  {
+    if (builtins.count(args[0]))
+    {
+      cout << args[0] << " is a shell builtin" << endl;
+      return;
+    }
+    string exe_path = find_executable(args[0], exec_dirs);
+    if (exe_path != "")
+    {
+      cout << args[0] << " is " << exe_path << endl;
+    }
+    else
+    {
+      cout << args[0] << ": not found" << endl;
+    }
+  }
+  else if (command == "exit")
+  {
+    if (args.size() == 0)
+      cerr << "exit code not provided" << endl;
+    else
+      exit(stoi(args[0]));
+  }
+  else if (command == "echo")
+  {
+    for (auto i : args)
+      cout << i << " ";
+    cout << endl;
+  }
+  else if (command == "cd")
+  {
+    if (args.size() == 0)
+      cerr << "cd: path not provided" << endl;
+    else if (args.size() > 1)
+      cerr << "cd: too many arguments" << endl;
+    else if (args[0] == "~")
+    {
+      chdir(getenv("HOME"));
+    }
+    else if (chdir(args[0].c_str()) != 0)
+      cerr << "cd: " << args[0] << ": No such file or directory" << endl;
+  }
 }
 
 int main()
@@ -23,14 +123,13 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  unordered_set<string>builtins{"echo","exit","type"};
-
   string env_variable = "PATH";
-  char* path= getenv(env_variable.c_str());
-  vector<string>exec_dirs;
+  char *path = getenv(env_variable.c_str());
+
   stringstream ss(path);
   string token;
-  while(getline(ss,token,':')){
+  while (getline(ss, token, ':'))
+  {
     exec_dirs.push_back(token);
   }
 
@@ -42,53 +141,39 @@ int main()
     string input;
     getline(cin, input);
 
-    stringstream s(input);
-    vector<string> arguments;
-    string argument;
-    while (s >> argument)
-    {
-      arguments.push_back(argument);
-    }
+    vector<string> arguments = parse_arguments(input);
 
-    if(arguments[0]=="exit"){
-      if(arguments.size() <=1) cerr<<"exit code not provided"<<endl;
-      if (arguments[1] == "0") return 0;
+    if (builtins.count(arguments[0]))
+    {
+      vector<string> args(arguments.begin() + 1, arguments.end());
+      run_builtin_commands(arguments[0], args);
     }
-    else if(arguments[0]=="echo"){
-      for(int i=1;i<arguments.size();i++) cout<<arguments[i]<<" ";
-      cout<<endl;
-    }else if(arguments[0]=="type"){
-      if(builtins.count(arguments[1])){
-        cout<<arguments[1]<<" is a shell builtin"<<endl;
-        continue;
-      }
-      string exe_path = find_executable(arguments[1],exec_dirs);
-      if(exe_path != ""){
-        cout<<arguments[1]<<" is "<<exe_path<<endl;
-      }else{
-        cout<<arguments[1]<<": not found"<<endl;
-      }
-    }
-    else{
-      string exe_path = find_executable(arguments[0],exec_dirs);
+    else
+    {
+
+      string exe_path = find_executable(arguments[0], exec_dirs);
       int pid = fork();
-      if(exe_path != "" && pid == 0){
+      if (exe_path != "" && pid == 0)
+      {
         // Run the executable
-        char* args[arguments.size() + 1];
-        for(size_t i = 0; i < arguments.size(); i++) {
-            args[i] = const_cast<char*>(arguments[i].c_str());
+        char *args[arguments.size() + 1];
+        for (size_t i = 0; i < arguments.size(); i++)
+        {
+          args[i] = const_cast<char *>(arguments[i].c_str());
         }
         args[arguments.size()] = nullptr;
         execvp(exe_path.c_str(), args);
         // If execvp returns, it means there was an error
         perror("execvp failed");
-        wait(nullptr);
         exit(1);
       }
-      if(exe_path == ""){
-        cout<<input<<": command not found"<<endl;
+      if (pid == 0)
+        return 0;
+      wait(nullptr);
+      if (exe_path == "")
+      {
+        cout << input << ": command not found" << endl;
       }
     }
-
   }
 }
