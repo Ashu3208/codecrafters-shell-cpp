b92964cb61f99113e58b5e3ebb48c82fa2d0f64e
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2025-02-27 08:31:09 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <sstream>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 int main()
 {
   // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
   bool end_program = false;
+  unordered_set<string> builtins={"echo","cd","pwd", "exit"};
   while (!end_program)
   {
     // Taking the user input
@@ -32,6 +34,12 @@ int main()
     else if(arguments[0]=="echo"){
       for(int i=1;i<arguments.size();i++) cout<<arguments[i]<<" ";
       cout<<endl;
+    }else if(arguments[0]=="type"){
+      if(builtins.find(arguments[1]) != builtins.end()){
+        cout<<arguments[1]<<" is a shell builtin"<<endl;
+      }else{
+        cout<<arguments[1]<<": not found"<<endl;
+      }
     }
     else{
       std::cout << input << ": command not found" << std::endl;
