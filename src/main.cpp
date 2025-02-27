7ad36b557b0ade44957c7f0ade6d0abce52555ec
Ashutosh Agrawal <ashutoshagrawal822@gmail.com>
2025-02-27 08:16:56 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,13 @@ int main()
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  while (1)
+  bool end_program = false;
+  while (!end_program)
   {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+    if(input == "exit")return 0;
     std::cout << input << ": command not found" << std::endl;
   }
 }
